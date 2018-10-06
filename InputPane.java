af8987f2bdfbb4cf786f04657ecb6c6eb6acd380
Azaria Fowler <37102959+ahfowler@users.noreply.github.com>
2018-10-06 11:58:22 -0700

    Add files via upload

---
+++ InputPane.java
@@ -0,0 +1,221 @@
+// Assignment #: Arizona State University CSE205 #6
+//         Name: Azaria Fowler
+//    StudentID: 1214875077
+//      Lecture: TTh 4:35pm-5:45pm
+//  Description: InputPane generates a pane where a user can enter
+//  a laptop information and create a list of available laptops.
+
+import java.util.ArrayList;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.RowConstraints;
+import javafx.scene.paint.Color;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
+import javafx.scene.text.Text;
+
+import javafx.geometry.Insets;
+import javafx.geometry.HPos;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+public class InputPane extends HBox {
+    private ArrayList<Laptop> laptopList;
+    private PurchasePane purchasePane;
+    
+    //Instantiating UI elements for easier accessing.
+    Label brandLabel;
+    TextField brandTF;
+    Label modelLabel;
+    TextField modelTF;
+    Label cpuLabel;
+    TextField cpuTF;
+    Label ramLabel;
+    TextField ramTF;
+    Label priceLabel;
+    TextField priceTF;
+    Text message = new Text("");
+    TextArea area = new TextArea("No laptops.");
+    
+    /* InputPane() - Constructor for class InputPane */
+    public InputPane(ArrayList<Laptop> list, PurchasePane pPane)
+    {
+        this.laptopList = list;
+        this.purchasePane = pPane;
+        
+        // Instantiating of scene layouts.
+        GridPane gridPane = new GridPane();
+        GridPane leftHalf = new GridPane();
+        GridPane textFields = new GridPane();
+        
+        // Left side of the application.
+        textFields.getRowConstraints().add(new RowConstraints(35));
+        textFields.getRowConstraints().add(new RowConstraints(35));
+        textFields.getRowConstraints().add(new RowConstraints(35));
+        textFields.getRowConstraints().add(new RowConstraints(35));
+        textFields.getRowConstraints().add(new RowConstraints(35));
+        textFields.getRowConstraints().add(new RowConstraints(35));
+        
+        textFields.add(brandLabel = new Label("Brand: "), 0, 0);
+        textFields.add(brandTF = new TextField(), 1, 0);   
+        textFields.add(modelLabel = new Label("Model: "), 0, 1);
+        textFields.add(modelTF = new TextField(), 1, 1);
+        textFields.add(cpuLabel = new Label("CPU (GHz): "), 0, 2);
+        textFields.add(cpuTF = new TextField(), 1, 2);
+        textFields.add(ramLabel = new Label("RAM (GB): "), 0, 3);
+        textFields.add(ramTF = new TextField(), 1, 3);
+        textFields.add(priceLabel = new Label("Price ($): "), 0, 4);
+        textFields.add(priceTF = new TextField(), 1, 4);
+        
+        Button enter = new Button("Enter a Laptop Info.");
+        
+        leftHalf.add(message, 0, 0);
+        leftHalf.add(textFields, 0, 1);
+        leftHalf.add(enter, 0, 2);
+        
+        leftHalf.getRowConstraints().add(new RowConstraints(30));
+        leftHalf.setHalignment(enter, HPos.CENTER);
+        leftHalf.setPadding(new Insets(10, 10, 10, 30));
+        
+        //Right side of the application.
+        VBox textArea = new VBox();
+        area.setPrefHeight(370);
+        textArea.getChildren().add(area);
+        
+        gridPane.getColumnConstraints().add(new ColumnConstraints(300)); // column 0 is 100 wide
+        gridPane.getColumnConstraints().add(new ColumnConstraints(300)); // column 1 is 200 wide
+        
+        gridPane.add(leftHalf, 0, 0); 
+        gridPane.add(textArea, 1, 0);
+        
+        //Add the left half and right half to the InputPane
+        this.getChildren().add(gridPane);
+        
+        //Register source object with event handler
+        ButtonHandler action = new ButtonHandler();
+        enter.setOnAction(action);
+        
+    }
+    
+    /* updater() - Whenever a laptop has been successfully added, this method will
+     * update the aggregated purchasePane.*/
+    public void updater() {
+        this.purchasePane.updateLaptopList();
+    }
+    
+    //Step #2: Create a ButtonHandler class
+    //ButtonHandler listens to see if the buttont "Enter a Laptop Info." is pushed or not,
+    //When the event occurs, it get a laptop's brand, model, CPU, RAM and price
+    //information from the relevant text fields, then create a new Laptop object and add it inside
+    //the laptopList. Meanwhile it will display the laptop's information inside the text area.
+    //It also does error checking in case any of the textfields are empty or wrong data was entered.
+    private class ButtonHandler implements EventHandler<ActionEvent>
+    {
+        /* textFieldsEmpty() - Helper function to check if any text fields are empty.*/
+        public boolean textFieldsEmpty() {
+            boolean textFieldsEmpty = false;
+            
+            if (brandTF.getText().isEmpty()) {
+                textFieldsEmpty = true;
+            } else if (modelTF.getText().isEmpty()) {
+                textFieldsEmpty = true;
+            } else if (cpuTF.getText().isEmpty()) {
+                textFieldsEmpty = true;
+            } else if (ramTF.getText().isEmpty()) {
+                textFieldsEmpty = true;
+            } else if (priceTF.getText().isEmpty()) {
+                textFieldsEmpty = true;
+            }
+            
+            return textFieldsEmpty;
+        }
+        
+        /* textFieldsWrong() - Helper function to check if any text fields are incorrectly formatted.*/
+        public boolean textFieldsWrong() {
+            boolean textFieldsWrong = false;
+            
+            try {
+                Double.parseDouble(cpuTF.getText());
+            } catch (NumberFormatException e) {
+                textFieldsWrong = true;
+            }
+            
+            try {
+                Double.parseDouble(ramTF.getText());
+            } catch (NumberFormatException e) {
+                textFieldsWrong = true;
+            }
+            
+            try {
+                Double.parseDouble(priceTF.getText());
+            } catch (NumberFormatException e) {
+                textFieldsWrong = true;
+            }
+            
+            return textFieldsWrong;
+        }
+        
+        /* inputPaneLaptops() - Helper function to traverses through the ArrayList of 
+         * laptop and returns a String version of it.*/
+        public String inputPaneLaptops(ArrayList list) {
+            String stringList = "";
+            for (int i = 0; i < list.size(); i++) {
+                stringList += list.get(i).toString();
+            }
+            return stringList;
+        }
+        
+        /* handle() - Overriding handle() abstract method so that it will check if textfields
+         * are empty, wrong, or create a duplicate. If not, then update ArrayList and then 
+         the right side of the application.*/
+        public void handle(ActionEvent e) {
+            if (textFieldsEmpty() || textFieldsWrong()) {
+                if (textFieldsEmpty()) {
+                    message.setText("Please fill all fields.");
+                    message.setFill(Color.RED);
+                } else {
+                    message.setText("Please enter correct data format.");
+                    message.setFill(Color.RED);
+                }
+            } else {
+                String brand = brandTF.getText();
+                String model = modelTF.getText();
+                Double cpu = Double.parseDouble(cpuTF.getText());
+                Double ram = Double.parseDouble(ramTF.getText());
+                Double price = Double.parseDouble(priceTF.getText());
+                
+                Laptop enteredLaptop = new Laptop(brand, model, cpu, ram, price);
+                
+                // Checking if the laptop is a duplicate.
+                boolean duplicateLaptop = false; 
+                for (int i = 0; i < laptopList.size(); i++) {
+                    if (laptopList.get(i).getBrand().equals(enteredLaptop.getBrand())
+                            && laptopList.get(i).getModel().equals(enteredLaptop.getModel())
+                            && laptopList.get(i).getCPU() == enteredLaptop.getCPU()
+                            && laptopList.get(i).getRAM() == enteredLaptop.getRAM()) {
+                        duplicateLaptop = true;
+                    }
+                }
+                
+                if (duplicateLaptop != true) {
+                    laptopList.add(enteredLaptop);
+                    message.setText("Laptop entered.");
+                    message.setFill(Color.BLACK);
+                    area.setText(inputPaneLaptops(laptopList));
+                    updater();
+                } else {
+                    message.setText("Duplicate laptop not entered.");
+                    message.setFill(Color.RED);
+                }
+            }
+        }
+    }   
+}
---
+++ PurchasePane.java
@@ -0,0 +1,162 @@
+// Assignment #: Arizona State University CSE205 #6
+//         Name: Azaria Fowler
+//    StudentID: 1214875077
+//      Lecture: TTh 4:35pm-5:45pm
+//  Description: PurchasePane displays a list of available laptops
+//  from which a user can select to buy. It also shows how many
+//  laptops are selected and what is the total amount.
+
+
+import javafx.scene.control.ListView;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.control.Button;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.event.ActionEvent; //**Need to import to handle event
+import javafx.event.EventHandler; //**Need to import to handle event
+import javafx.scene.layout.RowConstraints;
+
+import java.util.ArrayList;
+import java.text.DecimalFormat;
+import javafx.scene.layout.GridPane;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+
+public class PurchasePane extends VBox {
+    private ArrayList<Laptop> laptopList, selectedList;
+    private ListView<Laptop> laptopLV, selectedLV;
+    
+    Text availableLaptops = new Text("Available Laptops");
+    Text selectedLaptops = new Text("Selected Laptops");
+    int quantity = 0;
+    double amount = 0;
+    Text quantitySelected = new Text("Quantity Selected: " + quantity);
+    Text totalAmount = new Text("Total Amount: $" + amount);
+    Button pickLaptop = new Button("Pick a Laptop");
+    Button removeLaptop = new Button("Remove a Laptop");
+    ListView<Laptop> laptopListings;
+    ListView<Laptop> selectedListings;
+    
+    /* PurchasePane() - Constructor for class PurchasePane.*/
+    public PurchasePane(ArrayList<Laptop> list) {
+        this.laptopList = list;
+        selectedList = new ArrayList<Laptop>();
+        
+        this.laptopLV = new ListView<Laptop>();
+        this.selectedLV = new ListView<Laptop>();
+        
+        //Creating section for listed laptops.
+        VBox topHalf = new VBox();
+        topHalf.setPadding(new Insets(5, 15, 5, 15));
+        topHalf.setSpacing(10);
+        availableLaptops.setStyle("-fx-font-size: 14;");
+        topHalf.getChildren().addAll(availableLaptops, this.laptopLV);
+        
+        //Creating section for buttons.
+        HBox control = new HBox();
+        control.setAlignment(Pos.CENTER);
+        control.setSpacing(5);
+        control.setPadding(new Insets(15, 0, 15, 0));
+        control.getChildren().addAll(pickLaptop, removeLaptop);
+        
+        //Creating section for selected laptops.
+        VBox bottomHalf = new VBox();
+        bottomHalf.setPadding(new Insets(5, 15, 5, 15));
+        bottomHalf.setSpacing(10);
+        selectedLaptops.setStyle("-fx-font-size: 14;");
+        bottomHalf.getChildren().addAll(selectedLaptops, this.selectedLV);
+        
+        //Creating section for quantity and amount.
+        HBox information = new HBox();
+        quantitySelected.setStyle("-fx-font-size: 14;");
+        totalAmount.setStyle("-fx-font-size: 14;");
+        information.setAlignment(Pos.CENTER);
+        information.setSpacing(5);
+        information.setPadding(new Insets(15, 0, 15, 0));
+        information.getChildren().addAll(quantitySelected, totalAmount);
+        
+        this.getChildren().addAll(topHalf, control, bottomHalf, information);
+        
+        //Register the button with its handler class
+        ButtonHandler2 action = new ButtonHandler2();
+        pickLaptop.setOnAction(action);
+        removeLaptop.setOnAction(action);
+        
+    }
+    
+    /* updateLaptopList() - Updates list of available laptops on the top half.*/
+    public void updateLaptopList() {
+        ObservableList<Laptop> laptops = FXCollections.observableArrayList(this.laptopList);
+        this.laptopLV.setItems(laptops);
+        laptopListings = this.laptopLV;
+    }
+    
+    /* updateSelectedList() - Updates list of selected laptops on the top half.*/
+    public void updateSelectedList()
+    {
+        ObservableList<Laptop> laptops = FXCollections.observableArrayList(this.selectedList);
+        this.selectedLV.setItems(laptops);
+        selectedListings = this.selectedLV;
+    }
+    
+//Step #2: Create a ButtonHandler class
+    private class ButtonHandler2 implements EventHandler<ActionEvent>
+    {   
+        /* nullException() - Determines wether anything is selected in any of the ListViews.*/
+        public boolean nullException(ListView<Laptop> list) {  
+            boolean answer = true;
+            try {
+                list.getSelectionModel().getSelectedItem();
+            } catch (NullPointerException e) {
+                answer = false;
+            }
+            return answer;
+        }
+        
+        //Override the abstact method handle()
+        public void handle(ActionEvent e)
+        {
+            //If "Pick a Laptop" button is pressed AND you actually selected something from the top...
+            if (e.getSource().equals(pickLaptop) && nullException(laptopListings)) {
+                Laptop pickedLaptop1 = laptopListings.getSelectionModel().getSelectedItem();
+                if (!selectedList.contains(pickedLaptop1)) {
+                    
+                    //Update quantity
+                    selectedList.add(pickedLaptop1);
+                    quantity++;
+                    quantitySelected.setText("Quantity Selected: " + quantity);
+                    
+                    //Update amount
+                    amount += pickedLaptop1.getPrice();
+                    totalAmount.setText("Total Amount: $" + amount);
+                    
+                    updateSelectedList();
+                }
+            }
+            
+            //If "Remove a Laptop" button is pressed AND you actually selected something from the bottom...
+            else if (e.getSource().equals(removeLaptop) && nullException(selectedListings)) {
+                Laptop pickedLaptop2 = selectedListings.getSelectionModel().getSelectedItem();
+                if (selectedList.contains(pickedLaptop2)) {
+                    
+                    //Update quantity
+                    selectedList.remove(pickedLaptop2);
+                    quantity--;
+                    quantitySelected.setText("Quantity Selected: " + quantity);
+                    
+                    //Update amount
+                    amount -= pickedLaptop2.getPrice();
+                    totalAmount.setText("Total Amount: $" + amount);
+                    
+                    updateSelectedList();
+                }
+            } else {
+                //All invalid action, do nothing here;
+            }    
+        }
+    }
+} 
